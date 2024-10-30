aa6913e0a4d606c98f01b5f8fabbb383acaf368f
Felipe Antônio de O. Almeida <143608467+cc22130@users.noreply.github.com>
2024-10-30 11:07:22 -0300

    update project

---
+++ projeto_intermed_card/lib/card_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CardModel {
+  Image imagem; // imagem
+  String nome; // nome
+
+  CardModel({
+    required this.imagem, // imagem
+    required this.nome, // nome
+  });
+}
--- projeto_intermed_card/lib/main.dart
+++ projeto_intermed_card/lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_intermed_card/card_carousel.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carrossel de Cards - Monitores',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+              'Horários de Monitoria do DPD'), // Carrossel de Cards - Monitores
+          centerTitle: true,
+          titleTextStyle:
+              const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: CardCarousel(), // child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
+
+
+// teste:
+/*
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -18,3 +46,4 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+*/
