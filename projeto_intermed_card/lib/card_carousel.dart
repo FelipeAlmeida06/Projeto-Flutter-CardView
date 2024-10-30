2878b0b1261896a596c9913f46bbd4c4aefc251d
Felipe Antônio de O. Almeida <143608467+cc22130@users.noreply.github.com>
2024-10-30 10:59:48 -0300

    create project

---
+++ projeto_intermed_card/lib/card_carousel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'card_model.dart';
+
+class CardCarousel extends StatefulWidget {
+  @override
+  _CardCarouselState createState() => _CardCarouselState();
+}
+
+class _CardCarouselState extends State<CardCarousel> {}
